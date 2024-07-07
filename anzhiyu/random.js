fd6c1b4fde867f65a63711b08615233c2e6eb3a9
zj0078 <zj0078@qq.com>
2024-07-07 12:11:16 +0800

    Site updated: 2024-07-07 12:11:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/这是一篇新的博文/","2024/07/06/hello-world/"];function toRandomPost(){
+var posts=["2024/07/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
