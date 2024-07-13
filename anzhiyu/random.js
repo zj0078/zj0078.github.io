19a9ed544b071e91ce5b6f34acded91c0f0e92aa
zj0078 <zj0078@qq.com>
2024-07-14 07:44:25 +0800

    Site updated: 2024-07-14 07:44:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/wenzhang/PG-DBA-运维手册/","2024/wenzhang/hello-world/"];function toRandomPost(){
+var posts=["2024/wenzhang/vps一键root登录/","2024/wenzhang/PG-DBA-运维手册/","2024/wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
